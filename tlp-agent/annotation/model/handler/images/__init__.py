dbb5e25b0ab2a7e5a11f0ec6212e52fc80b865e5
dujiujun <dujiujun@hopehouse.com>
2019-12-18 15:51:34 +0800

    开放获取标签模板的接口

--- tlp-agent/annotation/model/handler/images/__init__.py
+++ tlp-agent/annotation/model/handler/images/__init__.py
@@ -4,10 +4,11 @@
 @Author: jerome.du
 @LastEditors: jerome.du
 @Date: 2019-11-04 11:20:41
-@LastEditTime: 2019-11-20 21:14:48
+@LastEditTime: 2019-12-18 15:51:16
 @Description:
 '''
 
 from .ImagesListHandler import ImagesListHandler
 from .LockImageHandler import LockImageHandler
 from .UnlockImageHandler import UnlocakImageHandler
+from .ImageLabelsHandler import ImageLabelsHandler
