8ef1e2137c5e193a9f52e5d4af93b0a51066be1f
dujiujun <dujiujun@hopehouse.com>
2020-03-17 15:40:27 +0800

    修正打包脚本路径错误

--- tlp-library/package.sh
+++ tlp-library/package.sh
@@ -4,4 +4,4 @@ rm -rf ./build ./dist ./TLPLibrary.egg-info TLPLibrary-1.0-py3-none-any.whl
 
 python ./steup.py bdist_wheel
 
-cp ./dist\TLPLibrary-1.0-py3-none-any.whl ./
+cp ./dist/TLPLibrary-1.0-py3-none-any.whl ./
